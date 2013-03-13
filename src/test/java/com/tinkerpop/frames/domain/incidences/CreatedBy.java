7f072df38f24577e6bd4ef4b33157d7407606b41
Mike Bryant <mikesname@gmail.com>
2013-03-13 11:52:42 +0000

    Cleanup.
    Remove unused imports and unnecessarily extended CreatedBy class.

--- src/test/java/com/tinkerpop/frames/domain/incidences/CreatedBy.java
+++ src/test/java/com/tinkerpop/frames/domain/incidences/CreatedBy.java
@@ -1,7 +1,6 @@
 package com.tinkerpop.frames.domain.incidences;
 
 import com.tinkerpop.frames.Domain;
-import com.tinkerpop.frames.EdgeFrame;
 import com.tinkerpop.frames.Property;
 import com.tinkerpop.frames.Range;
 import com.tinkerpop.frames.domain.classes.Person;
@@ -10,7 +9,7 @@ import com.tinkerpop.frames.domain.classes.Project;
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
-public interface CreatedBy extends EdgeFrame {
+public interface CreatedBy {
 
     @Domain
     public Project getDomain();
--- src/test/java/com/tinkerpop/frames/domain/incidences/WeightedEdge.java
+++ src/test/java/com/tinkerpop/frames/domain/incidences/WeightedEdge.java
@@ -1,11 +1,7 @@
 package com.tinkerpop.frames.domain.incidences;
 
-import com.tinkerpop.frames.Domain;
 import com.tinkerpop.frames.EdgeFrame;
 import com.tinkerpop.frames.Property;
-import com.tinkerpop.frames.Range;
-import com.tinkerpop.frames.domain.classes.Person;
-import com.tinkerpop.frames.domain.classes.Project;
 
 /**
  * @author Mike Bryant (https://github.com/mikesname)

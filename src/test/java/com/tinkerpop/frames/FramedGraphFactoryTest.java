cb6035817c71411e2975330d65f72978348c44b0
bryncooke <bryncooke@gmail.com>
2013-06-03 10:36:25 +0100

    Added unit test for wrapping in framed graph factory.

--- src/test/java/com/tinkerpop/frames/FramedGraphFactoryTest.java
+++ src/test/java/com/tinkerpop/frames/FramedGraphFactoryTest.java
@@ -2,45 +2,65 @@ package com.tinkerpop.frames;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.internal.stubbing.answers.ReturnsArgumentAt;
 
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.TransactionalGraph;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 
 public class FramedGraphFactoryTest {
 
+	private Module mockModule;
+
+
+	@Before
+	public void setup() {
+		mockModule = Mockito.mock(Module.class);
+	}
+	
 	@Test
 	public void testFactory() {
-		FramedGraphFactory graphFactory = FramedGraphFactory.createFactory(new GraphModule());
+		
+		Mockito.when(mockModule.configure(Mockito.any(Graph.class), Mockito.any(FramedGraphConfiguration.class))).then(new ReturnsArgumentAt(0));
+		FramedGraphFactory graphFactory = FramedGraphFactory.createFactory(mockModule);
 		TinkerGraph base = new TinkerGraph();
 		FramedGraph<TinkerGraph> framed = graphFactory.create(base);
 		Assert.assertEquals(base, framed.getBaseGraph());
 		
+		Mockito.verify(mockModule, Mockito.times(1)).configure(Mockito.any(Graph.class), Mockito.any(FramedGraphConfiguration.class));
+		
 		TransactionalTinkerGraph baseTransactional = new TransactionalTinkerGraph();
 		TransactionalFramedGraph<TransactionalTinkerGraph> framedTransactional = graphFactory.create(baseTransactional);
 		Assert.assertEquals(baseTransactional, framedTransactional.getBaseGraph());
-		
-		
 
+		Mockito.verify(mockModule, Mockito.times(1)).configure(Mockito.any(TransactionalGraph.class), Mockito.any(FramedGraphConfiguration.class));
 	}
 	
-	
-	public static class GraphModule implements Module {
+	@Test
+	public void testWrapping() {
+		Graph wrapper = Mockito.mock(Graph.class);
+		Mockito.when(mockModule.configure(Mockito.any(Graph.class), Mockito.any(FramedGraphConfiguration.class))).thenReturn(wrapper);
+		FramedGraphFactory graphFactory = FramedGraphFactory.createFactory(mockModule);
+		TinkerGraph base = new TinkerGraph();
+		FramedGraph<TinkerGraph> framed = graphFactory.create(base);
+		
+		Mockito.verify(mockModule).configure(Mockito.any(Graph.class), Mockito.any(FramedGraphConfiguration.class));
+		
+		//Unwrapping the graph should retrieve the base graph.
+		Assert.assertEquals(base, framed.getBaseGraph());
 
-		@Override
-		public Graph configure(Graph baseGraph, FramedGraphConfiguration config) {
-			return baseGraph;
-		}
-
-		@Override
-		public TransactionalGraph configure(TransactionalGraph baseGraph,
-				FramedGraphConfiguration config) {
-			return baseGraph;
-		}
-
+		//But using the framed graph should go through the wrappers installed by the module.
+		Vertex vertex = Mockito.mock(Vertex.class);
+		Mockito.when(wrapper.getVertex(1)).thenReturn(vertex);
+		Assert.assertEquals(vertex, framed.getVertex(1));
 		
 	}
+	
+
 	
 
 	public static class TransactionalTinkerGraph extends TinkerGraph implements TransactionalGraph {
